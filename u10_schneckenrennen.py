bde9167ca40f2a504eb03b15a808e6ce87750acb
CMT <you@example.com>
2021-06-02 13:06:13 +0200

    Tag Drei

--- u10_schneckenrennen.py
+++ u10_schneckenrennen.py
@@ -1,3 +1,5 @@
+# https://wiki.freitagsrunde.org/Javakurs/%C3%9Cbungsaufgaben/Rennschnecke
+
 from random import random
 
 
@@ -13,6 +15,7 @@ class Rennschnecke:
         self.position += strecke
 
     def to_string(self):
+        # __str__ als bessere Alternative
         return self.name + ' (' + self.rasse + ') ' + str(self.max_v) + ' === ' + str(self.position)
 
 
@@ -27,13 +30,56 @@ class Rennen:
         self.teilnehmer.append(schnecke)
         self.anzahl_teilnehmer += 1
 
+    def del_rennschnecke(self, name):
+        self.teilnehmer.remove(name)
+
+        for sn in self.teilnehmer:
+            if sn.name == name:
+                self.teilnehmer.remove(sn)
+                self.anzahl_teilnehmer -= 1
+                break
+
+    def to_string(self):
+        # __str__ als Alternative
+        text = self.name + ' (' + str(self.laenge) + ') \n'
+
+        for sn in self.teilnehmer:
+            text += sn.to_string() + '\n'
+
+        return text
+
+    def ermittle_gewinner(self):
+        for sn in self.teilnehmer:
+            if sn.position >= self.laenge:
+                return sn
+
+        return None
+
+    def lasse_schnecken_kriechen(self):
+        for sn in self.teilnehmer:
+            sn.krieche()
+
+    def durchfuehren(self):
+        # == -> is
+        while self.ermittle_gewinner() is None:
+            self.lasse_schnecken_kriechen()
+
+        return self.ermittle_gewinner()
 
 
-rudi = Rennschnecke('Rudi', 'Weinbergschnecke', 10)
-fritz = Rennschnecke('Fritz', 'Weinbergschnecke', 9)
-mariechen = Rennschnecke(' ', 'Weinbergschnecke', 11)
+s1 = Rennschnecke('Rudi', 'Weinbergschnecke', 10)
+s2 = Rennschnecke('Fritz', 'Weinbergschnecke', 9)
+s3 = Rennschnecke('Mariechen', 'Weinbergschnecke', 11)
 
 nuerburg = Rennen('Nürburg', 100)
-nuerburg.add_rennschnecke(rudi)
-nuerburg.add_rennschnecke(fritz)
-nuerburg.add_rennschnecke(mariechen)
+nuerburg.add_rennschnecke(s1)
+nuerburg.add_rennschnecke(s2)
+nuerburg.add_rennschnecke(s3)
+
+# Nicht die Schnecke
+# nuerburg.del_rennschnecke(rudi)
+# Nur der NAME rudi.name
+# nuerburg.del_rennschnecke('Rudi')
+
+nuerburg.durchfuehren()
+print(nuerburg.to_string())
