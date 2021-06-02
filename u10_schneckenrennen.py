67a05418e0ecacdb7c9e37670730c4ed2ac7d1af
CMT <you@example.com>
2021-06-02 13:30:28 +0200

    Zweiter Tag

--- u10_schneckenrennen.py
+++ u10_schneckenrennen.py
@@ -41,7 +41,7 @@ class Rennen:
 
     def to_string(self):
         # __str__ als Alternative
-        text = self.name + ' (' + str(self.laenge) + ') \n'
+        text = self.name + ' (' + str(self.laenge) + ')\n'
 
         for sn in self.teilnehmer:
             text += sn.to_string() + '\n'
